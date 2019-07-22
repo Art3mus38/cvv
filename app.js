4dfe3d0516ee0d006b8e4acb13310911d0541dd8
Art3mus38 <53157602+Art3mus38@users.noreply.github.com>
2019-07-21 20:43:53 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var showIngredientsOne = document.querySelector('.showIngredientsOne');
+var showIngredientsTwo = document.querySelector('.showIngredientsTwo');
+var showIngredientsThree = document.querySelector('.showIngredientsThree');
+var showIngredientsFour = document.querySelector('.showIngredientsFour');
+
+showIngredientsOne.addEventListener('click', () => {
+document.querySelector('.paragraphOne').innerHTML = "Devil's Fruit Sorbet";
+});
+showIngredientsTwo.addEventListener('click', () => {
+document.querySelector('.paragraphTwo').innerHTML = "Devil's Fruit Sorbet";
+});
+showIngredientsThree.addEventListener('click', () => {
+document.querySelector('.paragraphThree').innerHTML = "Devil's Fruit Sorbet";
+});
+showIngredientsFour.addEventListener('click', () => {
+document.querySelector('.paragraphFour').innerHTML = "Devil's Fruit Sorbet";
+});
